ab451be8b150d6d07129c0706857bfa749648bd9
Shashank Naik <shashanknaik808@gmail.com>
2023-03-28 11:46:25 +0530

    Javascript chapters till 50

---
+++ 02/02.js
@@ -0,0 +1,6 @@
+console.log("This code contains code for the Ultimate JavaScript course vid no 2");
+var a = 67 // a contaians 67
+console.log(a);
+a = "shashank"
+console.log(a)
+// Let 8shashank = 7  //Not allowed, this will throw an error
---
+++ 03/03.js
@@ -0,0 +1,15 @@
+console.log("JavaScript tutorial 03 : var, let and const");
+var a = 45;
+var a = "p"
+let b = "Shashank"
+const author = "Shashank"
+// author =  5 //Throws an error because constant cannot be changed
+b = 4
+const shashank = 0;
+let c = null
+let d = undefined 
+ {
+    let b = 'this'
+    console.log(b)
+ }
+console.log(b);
