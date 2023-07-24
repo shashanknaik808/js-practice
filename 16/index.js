1ce62e350713a4609f58547330e1556a97802cb0
Shashank Naik <shashanknaik808@gmail.com>
2023-07-24 16:04:03 +0530

    Creating, Removing & Replacing Elements

--- 16/index.js
+++ 16/index.js
@@ -1,8 +1,20 @@
-console.log('this is tut 16');
+/*console.log('this is tut 16');
 let element = document.createElement('li');
 let text = document.createTextNode('I am a text node');
 element.appendChild(text)
+*/
 
+let ele = document.querySelector('.container');
+
+let anc = document.createElement('h1');
+let tn = document.createTextNode('Visit Google Website');
+anc.appendChild(tn);
+let ret = anc.setAttribute('href', 'http://www.google.com');
+console.log(ret);
+
+ele.appendChild(anc);
+
+/*
 // Add a class name to the li element
 element.className = 'childul';
 element.id = 'createdLi';
@@ -14,6 +26,7 @@ let ul = document.querySelector('ul.this');
 ul.appendChild(element);
 // console.log(ul)
 // console.log(element)
+*/
 
 let elem2 = document.createElement('h3');
 elem2.id = 'elem2';
@@ -21,6 +34,10 @@ elem2.className = 'elem2';
 let tnode = document.createTextNode('This is a created node for elem2');
 elem2.appendChild(tnode);
 
+ele.replaceChild(elem2, document.querySelector('.item'));
+
+
+/*
 element.replaceWith(elem2);
 let myul = document.getElementById('myul');
 myul.replaceChild(element, document.getElementById('fui'));
@@ -29,6 +46,4 @@ let pr = elem2.hasAttribute('href');
 elem2.removeAttribute('id');
 elem2.setAttribute('title', 'myelem2title');
 console.log(elem2, pr);
-
-// quick quiz
-// create a heading element with text as "Go to CodeWithHarry" and create an a tag outside it with href = "https://www.codewithharry.com"
+*/
